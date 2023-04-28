716e9d749a9941c40f09f5c602435ad3e19aa996
Manthan Moharana <manthanmoharana03583@gmail.com>
2023-04-28 23:45:46 +0530

    updating review routes

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const {setReview, getAgentReviews} = require('../controllers/reviewController.js')
+const protect = require('../middleware/authMiddleware.js')
 
 router.route('/').post(protect,setReview)
 router.route('/:id').get(getAgentReviews)
